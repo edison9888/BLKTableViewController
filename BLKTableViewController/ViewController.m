5a548e6589269621ca39619ce91fa9e64e378665
blackgo <black.go@qq.com>
2013-01-24 12:38:21 +0800

    Update BLKTableViewController/ViewController.m

--- BLKTableViewController/ViewController.m
+++ BLKTableViewController/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self performSelector:@selector(fetchData) withObject:nil afterDelay:2.0];
 }
 
 - (void)didReceiveMemoryWarning
